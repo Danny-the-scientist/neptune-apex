7f7ed0d746b58d6c5025244f2262431ebf170752
Danny <96215591+Danny-the-scientist@users.noreply.github.com>
2022-01-01 16:27:02 -0500

    Create feed-seymour.vb

---
+++ feed-seymour.vb
@@ -0,0 +1,6 @@
+'as seen in wav controls, the virtual output of feeding seymour'
+
+Fallback OFF
+Set OFF
+If GrnBut CLOSED Then ON 'push the big green button to activate'
+Min Time 005:00 Then ON 'the feed cycle turns on and remains so for 5 minutes'
